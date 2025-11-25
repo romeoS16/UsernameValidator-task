71b9e2b833307022b0c3373cf26f1a9491a2e0dc
romeoS16 <romeoschweizer29@gmail.com>
2025-11-25 15:27:37 +0100

    results

--- src/main/java/UsernameValidator.java
+++ src/main/java/UsernameValidator.java
@@ -8,14 +8,29 @@ public class UsernameValidator {
      *  - er höchstens 15 Zeichen lang ist,
      *  - das erste Zeichen ein Buchstabe ist (a–z oder A–Z),
      *  - alle weiteren Zeichen nur aus Buchstaben, Ziffern oder Unterstrichen ('_') bestehen,
-     *  - keine Leerzeichen, Umlaute oder sonstige Sonderzeichen vorkommen.
+     *  - keine Leerzeichen oder sonstige Sonderzeichen vorkommen.
      *
      * @param name der zu prüfende Benutzername (darf nicht null sein)
      * @return true, wenn der Name alle Regeln erfüllt, sonst false
      */
 
     public static boolean isValidUsername(String name) {
-        // TODO: implementieren
-        return false;
+        if(name == null || name.isEmpty()){
+            return false;
+        }
+        if(name.length() < 3 || name.length() > 15){
+            return false;
+        }
+        if(!Character.isLetter(name.charAt(0))){
+            return false;
+        }
+        for(int i = 1; i < name.length(); i++){
+            if(!(Character.isLetter(name.charAt(i))
+                    || Character.isDigit(name.charAt(i))
+                    || name.charAt(i) == '_')){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/UsernameValidatorTest.java
+++ src/test/java/UsernameValidatorTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class UsernameValidatorTest {
@@ -10,5 +11,66 @@ public class UsernameValidatorTest {
         assertTrue(result);
     }
 
-    // TODO: weitere Testfälle ergänzen
+    @Test
+    public void minLengthValid_returnsTrue() {
+        assertTrue(UsernameValidator.isValidUsername("abc"));
+    }
+
+    @Test
+    public void belowMinLength_returnsFalse() {
+        assertFalse(UsernameValidator.isValidUsername("ab"));
+    }
+
+    @Test
+    public void maxLengthValid_returnsTrue() {
+        // 15 Zeichen
+        assertTrue(UsernameValidator.isValidUsername("abcdefghijklmno"));
+    }
+
+    @Test
+    public void aboveMaxLength_returnsFalse() {
+        // 16 Zeichen
+        assertFalse(UsernameValidator.isValidUsername("abcdefghijklmnop"));
+    }
+
+    @Test
+    public void startsWithDigit_returnsFalse() {
+        assertFalse(UsernameValidator.isValidUsername("1start"));
+    }
+
+    @Test
+    public void startsWithUnderscore_returnsFalse() {
+        assertFalse(UsernameValidator.isValidUsername("_user"));
+    }
+
+    @Test
+    public void containsSpace_returnsFalse() {
+        assertFalse(UsernameValidator.isValidUsername("max mustermann"));
+    }
+
+    @Test
+    public void containsSpecialCharacter_returnsFalse() {
+        assertFalse(UsernameValidator.isValidUsername("user!"));
+        assertFalse(UsernameValidator.isValidUsername("max-mustermann"));
+        assertFalse(UsernameValidator.isValidUsername("hallo&bin"));
+
+    }
+
+    @Test
+    public void validWithDigitsAndUnderscore_returnsTrue() {
+        assertTrue(UsernameValidator.isValidUsername("Alice_01"));
+        assertTrue(UsernameValidator.isValidUsername("User123"));
+    }
+
+    @Test
+    public void emptyString_returnsFalse() {
+        assertFalse(UsernameValidator.isValidUsername(""));
+    }
+
+    @Test
+    public void nullInput_returnsFalse() {
+        assertFalse(UsernameValidator.isValidUsername(null));
+    }
+
+
 }
